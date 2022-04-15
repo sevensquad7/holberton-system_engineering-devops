83a107a2b19a4a4b36bb530df75110d0d5f58bf3
zye7ert <edgarxot@gmail.com>
2022-04-15 17:42:07 -0500

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/env pup
 # Using Puppet, install puppet-lint.
 package { 'puppet-lint':
-  ensure    => '2.5.0'
-  provider  => 'gem'
-  source    => 'http://rubygems.org',
+  ensure    => '2.5.0',
+  provider  => 'gem',
+  source    => 'http://rubygems.org'
 }
