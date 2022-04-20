8a86acb374fed97f4270530fbc7204769da38118
zye7ert <edgarxot@gmail.com>
2022-04-20 10:37:32 -0500

    update task resolution

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,14 @@
 # Using Puppet, install puppet-lint.
 
-package { 'puppet-lint':
-    ensure   => '2.5.0',
-    name     => 'puppet-lint',
-    provider => 'gem',
-    source   => 'http://rubygems.org',
+#package { 'puppet-lint':
+#    ensure   => '2.5.0',
+#    name     => 'puppet-lint',
+#    provider => 'gem',
+#    source   => 'http://rubygems.org',
+#}
+
+package { 'flask':
+    ensure   => '2.1.0',
+    name     => 'flask',
+    provider => 'pip3',,
 }
